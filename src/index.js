a61e94f53470a3aa13d105ccc6d6ba85b490c752
greenkiwigreen <96068479+greenkiwigreen@users.noreply.github.com>
2022-02-04 04:49:38 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let str= n.toString();
+    let newStr=''; 
+    for (let i=0; i<str.length; i++){
+        newStr = str[i] + newStr; 
+    }
+    return parseInt(newStr);
 }
